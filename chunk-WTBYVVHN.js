7a4891acaf107fb7bd4eb32578c6427cc58e6280
zaricu22 <48631871+zaricu22@users.noreply.github.com>
2026-05-13 13:17:35 +0200

    Auto-generated commit

---
+++ chunk-WTBYVVHN.js
@@ -0,0 +1,1 @@
+import{c as r,d as n,u as a}from"./chunk-6TLKGECG.js";var s=class t{request$=new n;requests$=this.request$.asObservable();confirm(o){return new r(e=>{this.request$.next({message:o,resolve:i=>{e.next(i),e.complete()}})})}static \u0275fac=function(e){return new(e||t)};static \u0275prov=a({token:t,factory:t.\u0275fac,providedIn:"root"})};export{s as a};
