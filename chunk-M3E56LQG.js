160614c78dcc9538a5b12b7854495e40f1f24da9
zaricu22 <zaricu22@users.noreply.github.com>
2026-07-31 20:03:30 +0000

    deploy: 888c06adf64bc4ace14600d12add23687e24f26b

--- chunk-M3E56LQG.js
+++
@@ -1,1 +0,0 @@
-import{B as a,f as r,g as n}from"./chunk-3JVYVGPI.js";var s=class t{request$=new n;requests$=this.request$.asObservable();confirm(o){return new r(e=>{this.request$.next({message:o,resolve:i=>{e.next(i),e.complete()}})})}static \u0275fac=function(e){return new(e||t)};static \u0275prov=a({token:t,factory:t.\u0275fac,providedIn:"root"})};export{s as a};
